c9ccadc78f42ddc18b92650661b6c122339d3fa4
aldomddev <aldomddev@gmail.com>
2023-07-11 08:34:08 -0300

    adding coil disk cache

--- app/src/main/java/br/com/amd/rickandmorty/RickAndMortyApp.kt
+++ app/src/main/java/br/com/amd/rickandmorty/RickAndMortyApp.kt
@@ -3,6 +3,7 @@ package br.com.amd.rickandmorty
 import android.app.Application
 import coil.ImageLoader
 import coil.ImageLoaderFactory
+import coil.disk.DiskCache
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -11,6 +12,13 @@ class RickAndMortyApp: Application(), ImageLoaderFactory {
         return ImageLoader.Builder(this)
             .crossfade(true)
             .placeholder(R.drawable.placeholder)
+            .error(R.drawable.placeholder)
+            .diskCache {
+                DiskCache.Builder()
+                    .directory(this.cacheDir.resolve("image_cache"))
+                    .maxSizePercent(0.05)
+                    .build()
+            }
             .build()
     }
 }
